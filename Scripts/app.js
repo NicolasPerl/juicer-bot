7b2d497b93bbd3351942143e48d3664c3439d817
Nicolas Perlick <nicolasperlick@Nicolass-iMac.local>
2018-04-19 11:01:59 -0500

    comment fixed & bot won't comment on own post

--- Scripts/app.js
+++ Scripts/app.js
@@ -17,7 +17,156 @@ var juicer = angular.module('app', []);
 
   // comments controller
   juicer.controller('commentsC', function($scope, $location, $http) {
-    //$scope.commentContent = "testing";
+    //$scope.commentContent = " testing";
+    //set the counter 
+    var i = 0
+    var j = 0
+    var repeat_list = [];
+    var startTime = new Date().getTime();
+    
+    var commentGate = true;
+
+    startInterval = function () {
+      if (new Date().getTime() - startTime < 172800000) {//2 days
+        console.log('inside startInterval() if statement: !!!!!!!!!!!!!!!!!!!!!!!!!')
+        var myVar = setInterval(function(){  fetchPosts() }, 300000);//22000   604800000 1 week
+      }
+    }
+    
+
+    $scope.getVal=function(){
+      $scope.limit = $scope.limit_model;
+      window.limit = $scope.limit
+    }
+/*
+    $scope.getTag=function() {
+      //console.log('$scope.tag.tag_model: ', $scope.tag_model);
+      $scope.tag = $scope.tag_model;
+      window.tag = $scope.tag
+      console.log('tag: ', tag);
+      window.query = {
+        tag: tag,
+        limit: limit
+      };
+      
+    }
+  */  
+
+  //triggered after user hits submit button
+    $scope.fire=function() {
+      $scope.loading= true;
+      $scope.tag = $scope.tag_model;
+        window.tag = $scope.tag
+        console.log('tag: ', tag);
+        window.query = {
+          tag: tag,
+          limit: limit
+        };
+        fetchPosts();
+    }
+
+
+    //grab new posts from blockchain
+    function fetchPosts() {
+      steem.api.getDiscussionsByCreated(query, function(err, result) {
+        console.log('getDiscussionsByCreated: ',result);
+        window.stopAfter = result.length;
+        //itertate through result 
+        function inside() {
+          setTimeout(function () {
+            console.log('---------------inside set timeout: --------------');
+            window.permlinkSlug = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+            window.discussion = result[i];
+            console.log(i, discussion);
+            window.user_id = discussion.id;
+            //window.bodyText = discussion.body;
+            //console.log('bodyText: ', bodyText.length);
+
+            window.flag = $.inArray(user_id, repeat_list); //check if user_id is already in array
+    
+            if (flag === -1) {//
+              repeat_list.push(user_id);
+              window.permlink = discussion.permlink;
+              window.author = discussion.author;
+              commentCheck(result);
+            }
+            i++;
+            if (i == stopAfter) {
+              console.log("startInterval has been called !!!!!!!!!!!!!!!----------------");
+            }
+            if (i < stopAfter) {
+              inside();
+            }
+          }, 25000)
+        }
+        inside();
+      });
+    }
+
+  function commentCheck(result) {
+    steem.api.getContentReplies(author, permlink, function(err, result) {
+        if (!err) {
+          commentGate = true;
+          j = 0
+          var user = $scope.user.name;
+          $scope.$apply();
+          console.log('inside api.getContentReplies()--------------------------------');
+          console.log('result in commentCheck(): ',result);        
+          var checkID = result[j];
+          if (checkID != null) {            
+            console.log("result.length: ",);
+            for (j = 0; j < result.length; j++) { 
+                checkID = result[j];
+                console.log('j inside for loop: ',j);
+                var replyAuthor = checkID.author;//.id
+                //if replyAuthor matches with $user change commentGate flag
+                if (replyAuthor == user) {
+                  commentGate = false;
+                  console.log('if replyAuthor == user:----------------------- commentGate switched',);
+                }
+            }
+            console.log('author in commentcheck: ', author);
+            console.log('user in commentcheck: ', user);
+
+            if (author==user) {
+              console.log('inside if author==user');
+              commentGate = false;
+            }
+          }
+          if (commentGate) {
+            console.log('inside commentGate: ');
+            $scope.comment();
+          }
+        }
+      });
+  }
+
+  console.log('repeat_list: ',repeat_list);
+    /* auto comment*/
+    $scope.comment= function() {
+      $scope.loading= true;
+          var contentFinal = 'hey @'+author+$scope.content+'<br>';
+        //commentCheck();
+          /* broadcast comment
+          console.log('author in comment(): ',author);
+          console.log('permlink in comment(): ', permlink);
+          console.log('permlinkSlug in comment(): ', permlinkSlug);
+          console.log('$scope.user.name: ',$scope.user.name);*/
+          console.log('$scope.content: ', contentFinal);
+          sc2.comment(
+            author,//$scope.parentAuthor is what is was before 
+            permlink, //$scope.parentPermlink  21000
+            $scope.user.name, 
+            permlinkSlug, 
+            '',
+            contentFinal,
+            { tags: [tag] },
+            function(err, result) {
+              console.log("comment result: ", result);
+              //$scope.$apply();
+              console.log('<----------------------------comment fired-------------------------------->');
+            });
+    }    
   });
 
   // buy votes controller
@@ -78,124 +227,7 @@ var juicer = angular.module('app', []);
         }
     }
 
-    //set the counter 
-    var i = 0
-    var j = 0
-    var repeat_list = [];
-    var startTime = new Date().getTime();
     
-    var commentGate = true;
-
-    startInterval = function () {
-      if (new Date().getTime() - startTime < 172800000) {//2 days
-        console.log('inside startInterval() if statement: !!!!!!!!!!!!!!!!!!!!!!!!!')
-        var myVar = setInterval(function(){  fetchPosts() }, 300000);//22000   604800000 1 week
-      }
-    }
-    
-
-    $scope.getVal=function(){
-      $scope.limit = $scope.limit_model;
-      window.limit = $scope.limit
-    }
-/*
-    $scope.getTag=function() {
-      //console.log('$scope.tag.tag_model: ', $scope.tag_model);
-      $scope.tag = $scope.tag_model;
-      window.tag = $scope.tag
-      console.log('tag: ', tag);
-      window.query = {
-        tag: tag,
-        limit: limit
-      };
-      
-    }
-  */  
-
-  //triggered after user hits submit button
-    $scope.fire=function() {
-      $scope.loading= true;
-      $scope.tag = $scope.tag_model;
-        window.tag = $scope.tag
-        console.log('tag: ', tag);
-        window.query = {
-          tag: tag,
-          limit: limit
-        };
-        fetchPosts();
-    }
-
-
-    //grab new posts from blockchain
-    function fetchPosts() {
-      steem.api.getDiscussionsByCreated(query, function(err, result) {
-        console.log('getDiscussionsByCreated: ',result);
-        window.stopAfter = result.length;
-
-        //itertate through result 
-        function inside() {
-          setTimeout(function () {
-            console.log('---------------inside set timeout: --------------');
-            window.permlinkSlug = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-            window.discussion = result[i];
-            console.log(i, discussion);
-            window.user_id = discussion.id;
-            //window.bodyText = discussion.body;
-            //console.log('bodyText: ', bodyText.length);
-
-            window.flag = $.inArray(user_id, repeat_list); //check if user_id is already in array
-    
-            if (flag === -1) {//
-              repeat_list.push(user_id);
-              window.permlink = discussion.permlink;
-              window.author = discussion.author;
-              commentCheck(result);
-            }
-            i++;
-            if (i == stopAfter) {
-              console.log("startInterval has been called !!!!!!!!!!!!!!!----------------");
-            }
-            if (i < stopAfter) {
-              inside();
-            }
-          }, 25000)
-        }
-        inside();
-      });
-    }
-
-  function commentCheck(result) {
-    steem.api.getContentReplies(author, permlink, function(err, result) {
-        if (!err) {
-          commentGate = true;
-          j = 0
-          var user = $scope.user.name;
-          $scope.$apply();
-          console.log('inside api.getContentReplies()--------------------------------');
-          console.log('result in commentCheck(): ',result);        
-          var checkID = result[j];
-          
-          if (checkID != null) {            
-
-            console.log("result.length: ",);
-            for (j = 0; j < result.length; j++) { 
-                checkID = result[j];
-                console.log('j inside for loop: ',j);
-                var replyAuthor = checkID.author;//.id
-                //if replyAuthor matches with $user change commentGate flag
-                if (replyAuthor == user) {
-                  commentGate = false;
-                  console.log('if replyAuthor == user:----------------------- commentGate switched',);
-                }
-            }
-          }
-          if (commentGate) {
-            console.log('inside commentGate: ');
-            $scope.comment();
-          }
-        }
-      });
-  }
 
 
     // if ($scope.accessToken) {
@@ -230,29 +262,7 @@ var juicer = angular.module('app', []);
       });
     };
   
-    console.log('repeat_list: ',repeat_list);
-    /* auto comment*/
-    $scope.comment= function() {
-      $scope.loading= true;
-        //commentCheck();
-          /* broadcast comment*/
-          console.log('author in comment(): ',author);
-          console.log('permlink in comment(): ', permlink);
-          sc2.comment(
-            author,//$scope.parentAuthor is what is was before 
-            permlink, //$scope.parentPermlink  21000
-            $scope.user.name, 
-            permlinkSlug, 
-            '',
-            $scope.content,
-            { tags: [tag] },
-            function(err, result) {
-              console.log("comment result: ", result);
-              //$scope.$apply();
-              console.log('<----------------------------comment fired-------------------------------->');
-              
-            });
-    }    
+    
     // after 1 week stop
     //setTimeout(() => { clearInterval(timerId); alert('stop'); },  604800000);
 
